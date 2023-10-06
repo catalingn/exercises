59dcf83c6842ff53c8af7302d9076cf9c43d4f87
catalingn <142152997+catalingn@users.noreply.github.com>
2023-10-06 14:58:11 +0300

    Update index.test.jsx

--- overrides/app/pages/youtube/index.test.jsx
+++ overrides/app/pages/youtube/index.test.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Youtube from './index'; // Replace with the correct import path
+import Youtube from './index'; 
 
 test('Youtube component renders with the correct video URL', () => {
   const { container } = render(<Youtube />);
   const iframeElement = container.querySelector('iframe');
   expect(iframeElement).toBeDefined();
   
-  // Check the src attribute of the iframe
+ 
   const srcAttribute = iframeElement.getAttribute('src');
   expect(srcAttribute).toBe('https://www.youtube.com/embed/TswL6KhQDn4?si=QN0FL-9fFG6cgebs');
 });
